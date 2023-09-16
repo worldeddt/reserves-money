32eff89ff5260929ea27671266cfad54abb157b7
김태형 <ktest92@gmail.com>
2023-09-16 14:18:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# 사용자 적립금 관리 API
+
+---
+
+## Version
+`0.0.1`
+
+## Requirements
+
+spring boot 3.1.3 \
+jdk 17 \
+mariadb
+
+## Development
+
+#### Before
+```
+docker 설치
+postman 사용을 권장
+```
---
+++ src/main/java/money/ReservesMoneyApplication.java
@@ -0,0 +1,13 @@
+package money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservesMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservesMoneyApplication.class, args);
+    }
+
+}
